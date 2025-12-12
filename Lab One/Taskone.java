64e1bc63143269917fdd23509f69862de3c5db08
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-12 20:08:23 +0600

    Update Taskone.java

--- Lab One/Taskone.java
+++ Lab One/Taskone.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Isteak Ahmad";
+        String studentId = "E251016";
+        String email = "isteakahmad@gmail.com";
+        String phone = "+8801541258963";
+        int age = 21;
+        String address = "Noyabazar,Halishahar,Chattogram";
+
+        System.out.println("Personal Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age: " +age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
 
