f592353e5b4b98161a5aca5b655cc1c3dad48b55
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:27:30 +0600

    Update TaskOne.java

--- Lab Six/TaskOne.java
+++ Lab Six/TaskOne.java
@@ -1,1 +1,33 @@
+public class Main
+{ 
+    public static void main(String[] args) 
+    { 
+        Son son = new Son();
+        son.company(); 
+        son.car(); 
+        son.ownProperty();
+    }
+}
+class Grandfather 
+{
+     void company() 
+     {
+System.out.println("Grandfather's company: Bonoful Sweets");
+}
+}
+class Father extends Grandfather 
+{
+     void car() 
+     {
+          System.out.println("Father's car: Toyota");
+     }
+}
+class Son extends Father 
+{
+     void ownProperty() 
+     {
+          System.out.println("Son's property: Bicycle");
+     }
+}
+
 
