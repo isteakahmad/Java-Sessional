06e0aa38fbd168947d8b329b6ccfae5fca7c1b00
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:16:24 +0600

    Update Taskone.java

--- Lab Two/Taskone.java
+++ Lab Two/Taskone.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter student's marks: ");
+        int marks = input.nextInt();
+
+        if (marks >= 80 && marks <= 100) {
+            System.out.println("Grade: A+");
+        }
+        else if (marks >= 70 && marks <= 79) {
+            System.out.println("Grade: A");
+        }
+        else if (marks >= 60 && marks <= 69) {
+            System.out.println("Grade: B");
+        }
+        else if (marks >= 50 && marks <= 59) {
+            System.out.println("Grade: C");
+        }
+        else if (marks >= 40 && marks <= 49) {
+            System.out.println("Grade: D");
+        }
+        else if (marks < 40 && marks >= 0) {
+            System.out.println("Grade: Fail");
+        }
+        else {
+            System.out.println("Invalid marks. Please enter between 0–100.");
+        }
+    }
+}
 
