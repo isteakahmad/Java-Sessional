0511c05cdad065127edbb811e76c474451645210
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:29:53 +0600

    Update TaskOne.java

--- Lab Seven/TaskOne.java
+++ Lab Seven/TaskOne.java
@@ -1,1 +1,34 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Shape {
+    double area(double length, double width) {
+        return length * width;
+    }
+    double area(double radius) {
+        return 3.1416 * radius * radius;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+        System.out.println("Add (int,int): " + c.add(10, 20));
+        System.out.println("Add (double,double): " + c.add(5.5, 3.3));
+        System.out.println("Add (int,int,int): " + c.add(1, 2, 3));
+
+        Shape s = new Shape();
+        System.out.println("Rectangle Area: " + s.area(5, 10));
+        System.out.println("Circle Area: " + s.area(7));
+    }
+}
 
