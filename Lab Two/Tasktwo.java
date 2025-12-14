641bfa1062a47596d44703292a8ca90bc919e8af
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:18:05 +0600

    Update Tasktwo.java

--- Lab Two/Tasktwo.java
+++ Lab Two/Tasktwo.java
@@ -1,1 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+}
 
