2b2144f696eebf9c8664f95da074a6c458c4f367
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:31:57 +0600

    Update TaskOne.java

--- Lab Eight/TaskOne.java
+++ Lab Eight/TaskOne.java
@@ -1,1 +1,30 @@
+class Animal {
+    void sound() {
+        System.out.println(" generic sound");
+    }
+}
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.sound(); 
+        dog.sound();    
+        cat.sound();    
+    }
+}
+
 
