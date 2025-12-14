0750c1d0c7b18ebb883557cd98123e6a1d1df49f
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:30:33 +0600

    Update TaskTwo.java

--- Lab Seven/TaskTwo.java
+++ Lab Seven/TaskTwo.java
@@ -1,1 +1,16 @@
+class Shape {
+    double area(double length, double width) {
+        return length * width;
+    }
+    double area(double radius) {
+        return 3.1416 * radius * radius;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Shape s = new Shape();
+        System.out.println("Rectangle Area: " + s.area(15, 5));
+        System.out.println("Circle Area: " + s.area(8));
+    }
+}
 
