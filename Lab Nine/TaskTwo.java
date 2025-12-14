fb3b4b11d2c730bae67320d036fdb1611b0ca583
Isteak Ahmad <isteakahmadfoysal@gmail.com>
2025-12-15 01:34:33 +0600

    Update TaskTwo.java

--- Lab Nine/TaskTwo.java
+++ Lab Nine/TaskTwo.java
@@ -1,1 +1,49 @@
+class Student {
+    private String name;
+    private String id;
+    private double cgpa;
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.setName("Isteak");
+        s.setId("E251016");
+        s.setCgpa(3.56);
+
+        System.out.println("Student Information:");
+        System.out.println("Name: " + s.getName());
+        System.out.println("ID: " + s.getId());
+        System.out.println("CGPA: " + s.getCgpa());
+    }
+}
 
